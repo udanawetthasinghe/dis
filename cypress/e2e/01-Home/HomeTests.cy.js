0be9544bb011372d7642d4ec17673c37db849376
Nishan Silva <nishaan.silva@gmail.com>
2025-03-11 21:24:24 +0200

    cypress

--- cypress/e2e/01-Home/HomeTests.cy.js
+++ cypress/e2e/01-Home/HomeTests.cy.js
@@ -2,7 +2,7 @@ import Locators from '../../PageObjects/PageElements/HomeLocators.json';
 
 describe('Home Page Load Test', () => {
   it('should load the home page and display main elements', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/');
   //Assertion
   cy.get(Locators.mainImage).should('be.visible');
   cy.get(Locators.signInBtn).should('be.visible');
