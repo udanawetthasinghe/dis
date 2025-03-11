5b90ff09a603fb17cbbb50923452d5aa78d1ef0f
Nishan Silva <nishaan.silva@gmail.com>
2025-03-11 06:32:35 +0200

    cypress

--- cypress/e2e/01-Home/home.cy.js
+++ cypress/e2e/01-Home/home.cy.js
@@ -1,9 +1,11 @@
+import Locators from '../../PageObjects/PageElements/HomeLocators.json';
+
 describe('Login', () => {
   it('with', () => {
     cy.visit('http://localhost:3000/')
   //Assertion
-  cy.get('[data-testid="mainImage"]').should('be.visible');
-  cy.get('[data-testid="signInBtn"]').should('be.visible');
-  cy.get('[data-testid="signUpBtn"]').should('be.visible');
+  cy.get(Locators.mainImage).should('be.visible');
+  cy.get(Locators.signInBtn).should('be.visible');
+  cy.get(Locators.signUpBtn).should('be.visible');
   })
 })
