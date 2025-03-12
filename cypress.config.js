6835103e8dc545733150c15a639f393a4764ba30
Nishan Silva <nishaan.silva@gmail.com>
2025-03-12 21:30:03 +0200

    cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000", // Set your base URL here
+    setupNodeEvents(on, config) {
+      require('cypress-mongodb')(on);
+      return config;
+    },
   },
 });
