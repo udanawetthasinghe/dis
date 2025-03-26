77222ab33bedbc2bbe8b15c51dc4dd0401bc855a
Nishan Silva <nishaan.silva@gmail.com>
2025-03-26 21:17:36 +0200

    adding forcast docker image to ci pipline

--- python/app.py
+++ python/app.py
@@ -64,4 +64,4 @@ def dengue_cases_comp():
     return jsonify(load_data_comparison())
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
